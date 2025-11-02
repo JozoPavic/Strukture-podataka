455ff29f52a284a43fbf9d243209a110deb19a51
JozoPavic <YpgCo@JOZO>
2025-11-02 10:06:12 +0100

    dodaj komentare

--- Vjezba5/Vjezba5/z.c
+++ Vjezba5/Vjezba5/z.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #define N 10
-
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE -1
 typedef struct postfix* Position;
 typedef struct postfix {
-    double number;
+    int number;
     Position Next;
 }postfix;
 
@@ -19,67 +21,142 @@ free(temp)
 Nikad u fun(&head)
 head=head->Next radije Q=head->Next korisit zamjenski pokazivac
 
-5.zad
-infix (4+3)*2
-postfix zapis   43+2*
-
-u listu ubacujes brojeve kada dođe mat.operacija onda nju obavis
-Na kraju programa samo 1 broj i da nema viška mat.operacija
 */
-int fileRead();
+int fileCheck();
+int fileRead(Position);
+int push(Position,int);
+int pop(Position, Position,char);
+int out(Position);
 int main() {
 
     postfix Head = { 0,NULL };
-    /*1funkcija
-    Otvori datoteku
-    pohranit u podatke
-    zatvorit
+    fileCheck();
+    fileRead(&Head); 
+    out(Head.Next); 
 
-    AKO JE USPJESNO return EXIT_SUCCESS ako ne EXIT FAILURE
-    */
+    
+    return EXIT_SUCCESS;
+}
+int fileCheck() {
 
-    /*2 P
-    if
-    je li znak br
-    je li mat.operacija
-    else ima višak
+    FILE *f = fopen("C:\\Users\\YpgCo\\source\\repos\\JozoPavic\\Strukture-podataka\\Vjezba5\\postifx.txt","r");
+    if (f == NULL) { 
+        return -1; }
+    char *c=(char*)malloc(sizeof(char)*7);
+    int numberOfNumbers = 0,dum;
+    int numberOfOperations = 0;
 
-    push(ubacuje el na vrh ADDBEFORE(num,&head))
-    pop(&first,&second)-mos napravit da skida 2 el odjednom pa onda odradi mat op
-    ili
-    pop(&first,)-mos napravit da skida 1 el
-    */
+    while (feof(f) == 0) {
+        
+        fscanf(f,"%s",c);
+        printf("%s\n", c);
+        if (c == EOF) {
 
-    /*
-    Alocirat prostor za novi el
-    Position newEl=(Position)malloc(sizeof(postfix));//Umjesto head koristi zamjensku var Q
-    newEl->Next=head->Next
-    head->next=newEl;
-
-
-    Kad dođe matop korisit temp za novu listu ili u jedan od 2 el pohraniš rezultat pa onda obrišeš jedan od elemenata
-
-    first=head->Next
-    second=first->Next
-
-    napravis funkciju koja ce prepoznat znak pr +
-    first->number=first->number+second->number;
-    nesmis dobit neg?
-
-    nakon sto izracunas
-    pop(&first,&second)
-
-    pop
-    {first->Next=Second->Next;
-    Second->Next=NULL;
-    free(Second);
-
-    PREGLEDAJ DODATNE EL I IZBRISI
+        }
+        else if (sscanf(c, "%d",&dum)==1) {
+            numberOfNumbers++;
+        }
+        else {
+            if (*c == '+' || *c == '*' || *c == '-' || *c == '/' || *c== '%') {
+                numberOfOperations++;
+            }
+            else{
+                return EXIT_FAILURE;
+            }
+        }
+        
     }
-    */
+    if (numberOfOperations + 1 != numberOfNumbers) {
+        return EXIT_FAILURE;
+    }
+    printf("Numbers = %d Mat=%d", numberOfNumbers, numberOfOperations);
+    free(c);
+    fclose(f);
+    return EXIT_SUCCESS;
+}
+int fileRead(Position Start) {
+    FILE* f = fopen("C:\\Users\\YpgCo\\source\\repos\\JozoPavic\\Strukture-podataka\\Vjezba5\\postifx.txt", "r");
+    if (f == NULL) {
+        return -1;
+    }
+    Position Q = Start;
+    char* c = (char*)malloc(sizeof(char) * 7);
+    int numberOfNumbers = 0,dum;
+    
+    while (feof(f) == 0) {
+
+        fscanf(f,"%s", c);
+
+        if (c == EOF) {
+
+        }
+        else if (sscanf(c, "%d", &dum) == 1) {
+            numberOfNumbers++;
+            push(Q, dum);
+        }
+        else {
+            if (numberOfNumbers > 1) {
+              pop(Q->Next, Q->Next->Next, *c);
+
+            }
+            else {
+                printf("Nije postfix\n");
+                return EXIT_FAILURE;
+            }
+             
+        }
+    }
+    free(c);
+    fclose(f);
+    return EXIT_SUCCESS;
+}
+int push(Position Start,int num) {
+
+    Position Q = (Position)malloc(sizeof(postfix));
+    if (Q == NULL) {
+        return -1;
+    }
+    Q->number = num;
+    Q->Next = Start->Next;
+    Start->Next = Q;
+
     return 0;
 }
-int fileRead() {
+int pop(Position First, Position Second,char c) {
+    printf("\n%d", First->number);
+    printf("\n%d", Second->number);
+    if (c == '+') {
+       First->number = First->number + Second->number;
+    }
+    if (c == '*') {
+       First->number = First->number * Second->number;
+    }
+    if (c == '-') {
+        First->number = First->number - Second->number;
+    }
+    if (c == '%') {
+        First->number = First->number % Second->number;
+    }
+    if (c == '/') {
+        First->number = First->number / Second->number;
+    }
+    First->Next = Second->Next;
+    Second->Next = NULL;
+    free(Second);
+    return 0;
+}
+int out(Position Q) {
+    if (Q == NULL) {
+    
+    }
+    printf("\nRezultat: ");
+    while (Q != NULL) {
+       
+        printf("%d", Q->number);
+        Q = Q->Next;
+    }
+
+    free(Q);
 
     return 0;
 }
